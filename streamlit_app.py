405245aca318b47ff1fda839f4a86fc67e3ef71a
xyang0101 <151397119+xyang0101@users.noreply.github.com>
2023-11-28 11:02:46 -0600

    Update streamlit_app.py
    Add some detailed info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
-streamlit.title('My Parent')
+
 streamlit.title('My Parent New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3')
+streamlit.text('Kale')
+streamlit.text('Egg')
