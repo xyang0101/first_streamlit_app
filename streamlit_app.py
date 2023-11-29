f89bd1bbd1c00d046a2a813cd0483bee01431a61
xyang0101 <151397119+xyang0101@users.noreply.github.com>
2023-11-28 22:19:28 -0600

    Update streamlit_app.py
    Add fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3')
 streamlit.text('Kale')
 streamlit.text('Egg')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
